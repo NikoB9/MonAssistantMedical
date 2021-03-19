eb29002639dba2207b9c4320d4268008f985855b
Nico <nicolas.bourneuf.sio@gmail.com>
2021-03-19 18:41:19 +0100

    correction du menu

--- front/src/app/accueil/accueil.component.ts
+++ front/src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Menu} from "../models/menu.model";
 
 @Component({
   selector: 'app-accueil',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  navElems: Menu;
+
+  constructor() {
+    this.navElems = {accueil: true, releves: false, analyses: false, profil: false};
+  }
 
   ngOnInit(): void {
 
