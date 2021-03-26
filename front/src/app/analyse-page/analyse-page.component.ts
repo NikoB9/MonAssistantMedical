4e68445c6f1d6c3b1a7e22c7775a7b7e8b4132d9
TonyLim <tony-lim@hotmail.fr>
2021-03-26 16:42:29 +0100

    select type added

--- front/src/app/analyse-page/analyse-page.component.ts
+++ front/src/app/analyse-page/analyse-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Menu} from '../models/menu.model';
+import {TypeReleveService} from '../services/type-releve.service';
+import {TypeReleve} from '../models/typeReleve.model';
 
 @Component({
   selector: 'app-analyse-page',
@@ -8,17 +10,26 @@ import {Menu} from '../models/menu.model';
 })
 export class AnalysePageComponent implements OnInit {
 
+  typeReleves: TypeReleve[];
   navElems: Menu;
   connected: boolean;
   errorMessage: string;
 
-  constructor() {
+  constructor(private typeReleveService: TypeReleveService) {
+    this.typeReleves = [];
     this.navElems = {accueil: false, releves: false, analyses: true, profil: false};
     this.connected = sessionStorage?.getItem('id') !== null;
     this.errorMessage = 'Vous êtes déconnecté. Veuillez vous connecter afin d\'accéder à cette page';
   }
 
   ngOnInit(): void {
+  	this.getTypeReleves();
+  }
+
+  getTypeReleves(): void{
+    this.typeReleveService.getTypeReleves().subscribe((typeReleves) => {
+      this.typeReleves = typeReleves;
+    });
   }
 
 }
