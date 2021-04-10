d42272fc71412f8cf3f973781186b397f89afd6f
Nicolas BOURNEUF <nicolas.bourneuf.sio@gmail.com>
2021-04-10 13:22:40 +0200

    prod

--- front/src/app/services/analyse.service.ts
+++ front/src/app/services/analyse.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Analyse} from '../models/analyse.model';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { VariablesGlobales } from './global';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class AnalyseService {
   constructor(private http: HttpClient) { }
 
   public getAnalyseByReleveType(id: number | null): Observable<Analyse[]> {
-    return this.http.get<Analyse[]>(`http://localhost:3000/api/analyse/typeReleve/${id}`);
+    return this.http.get<Analyse[]>(`${VariablesGlobales.baseURL}analyse/typeReleve/${id}`);
   }
 }
