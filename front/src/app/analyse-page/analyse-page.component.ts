88a4e4bafaf09ab044afec55782fb2fe5bbc855f
Nicolas BOURNEUF <nicolas.bourneuf.sio@gmail.com>
2021-04-10 12:25:37 +0200

    refactorisation et service worker

--- front/src/app/analyse-page/analyse-page.component.ts
+++ front/src/app/analyse-page/analyse-page.component.ts
@@ -40,7 +40,7 @@ export class AnalysePageComponent implements OnInit {
       animation: {
         duration: ANIMATION_DURATION,
         startup: true,
-        easing: "inAndOut"
+        easing: 'inAndOut'
       },
     }
   };
@@ -89,7 +89,7 @@ export class AnalysePageComponent implements OnInit {
         animation: {
           duration: ANIMATION_DURATION,
           startup: true,
-          easing: "inAndOut"
+          easing: 'inAndOut'
         },
       }
     };
@@ -98,15 +98,15 @@ export class AnalysePageComponent implements OnInit {
   }
 
   getReleves(): void {
-    this.cursor("wait");
+    this.cursor('wait');
     this.utilisateurService.getUserReleves(this.id).subscribe((releves) => {
-      this.cursor("initial");
+      this.cursor('initial');
       releves.complexesReleves.forEach(complexeReleve => {
-        const idType: number = complexeReleve.releve.TypeReleve.id
-        const releve: Releve = complexeReleve.releve
+        const idType: number = complexeReleve.releve.TypeReleve.id;
+        const releve: Releve = complexeReleve.releve;
         if (!this.typeReleves.some(e => e.id === idType)) {
           if (this.idTypeSelected === '-1') {
-            this.idTypeSelected = "" + idType;
+            this.idTypeSelected = '' + idType;
           }
           this.typeReleves.push(releve.TypeReleve);
           this.chartValues[idType] = {
@@ -121,58 +121,58 @@ export class AnalysePageComponent implements OnInit {
                 lineWidth: 3
               }
             }
-          }
+          };
         }
         const splitedDate = releve.prise_de_mesure.split('-');
-        const date = splitedDate[1] + "/" + splitedDate[2].substr(0,2) + "/" + splitedDate[0]
-        this.chartValues[idType]['data'].push([new Date(date), releve.valeur]);
+        const date = splitedDate[1] + '/' + splitedDate[2].substr(0, 2) + '/' + splitedDate[0];
+        this.chartValues[idType].data.push([new Date(date), releve.valeur]);
       });
       this.idTypeSelected = this.analyseForm.value.TypeReleveId;
       this.applyChartValues();
       this.typeReleves.forEach(typeReleve => {
         this.analyseService.getAnalyseByReleveType(typeReleve.id).subscribe((analyses) => {
           let count = 1;
-          for (let i in analyses) {
+          for (const i in analyses) {
             const analyse = analyses[i];
-            this.chartValues[typeReleve.id]['columns'].push(analyse.Dangerosite.message);
-            this.chartValues[typeReleve.id]['colors'].push(analyse.Dangerosite.Couleur.label);
-            this.chartValues[typeReleve.id]['series'][count++] = { lineWidth: 1 };
-            if (analyse.Dangerosite.Couleur.id != RED_COLOR_ID) {
-              this.chartValues[typeReleve.id]['columns'].push(analyse.Dangerosite.message);
-              this.chartValues[typeReleve.id]['colors'].push(analyse.Dangerosite.Couleur.label);
-              this.chartValues[typeReleve.id]['series'][count++] = { lineWidth: 1 };
+            this.chartValues[typeReleve.id].columns.push(analyse.Dangerosite.message);
+            this.chartValues[typeReleve.id].colors.push(analyse.Dangerosite.Couleur.label);
+            this.chartValues[typeReleve.id].series[count++] = { lineWidth: 1 };
+            if (analyse.Dangerosite.Couleur.id !== RED_COLOR_ID) {
+              this.chartValues[typeReleve.id].columns.push(analyse.Dangerosite.message);
+              this.chartValues[typeReleve.id].colors.push(analyse.Dangerosite.Couleur.label);
+              this.chartValues[typeReleve.id].series[count++] = { lineWidth: 1 };
             }
-            this.chartValues[typeReleve.id]['data'].forEach(d => {
-              if (analyse.Dangerosite.Couleur.id == RED_COLOR_ID) {
-                if (analyse.mini == 0) {
-                  d.push(analyse.maxi)
+            this.chartValues[typeReleve.id].data.forEach(d => {
+              if (analyse.Dangerosite.Couleur.id === RED_COLOR_ID) {
+                if (analyse.mini === 0) {
+                  d.push(analyse.maxi);
                 } else {
-                  d.push(analyse.mini)
+                  d.push(analyse.mini);
                 }
               } else {
-                d.push(analyse.mini)
-                d.push(analyse.maxi)
+                d.push(analyse.mini);
+                d.push(analyse.maxi);
               }
             });
           }
-        })
+        });
       });
     });
   }
 
-  changeType() {
+  changeType(): void {
     this.idTypeSelected = this.analyseForm.value.TypeReleveId;
     this.applyChartValues();
   }
 
   applyChartValues(): void {
-    this.chart.data = this.chartValues[this.idTypeSelected]['data'];
-    this.chart.columns = this.chartValues[this.idTypeSelected]['columns'];
-    this.chart.options['colors'] = this.chartValues[this.idTypeSelected]['colors'];
-    this.chart.options['series'] = this.chartValues[this.idTypeSelected]['series'];
+    this.chart.data = this.chartValues[this.idTypeSelected].data;
+    this.chart.columns = this.chartValues[this.idTypeSelected].columns;
+    this.chart.options.colors = this.chartValues[this.idTypeSelected].colors;
+    this.chart.options.series = this.chartValues[this.idTypeSelected].series;
   }
 
-  cursor(cursorType: string) {
-    document.getElementsByTagName("body")[0].style.cursor = cursorType;
+  cursor(cursorType: string): void {
+    document.getElementsByTagName('body')[0].style.cursor = cursorType;
   }
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -67,7 +67,12 @@ const appRoutes: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     GoogleChartsModule,
-    ServiceWorkerModule.register('../ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     UtilisateurService,
